6b6774f72f57534aa74e3179d3c05f41bafee5e3
Welber Camargos <welber-camargos@hotmail.com>
2020-06-30 17:33:13 -0300

    PROJECT CREATE

---
+++ src/main/java/com/wnweb/curso/CursoJavaUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.wnweb.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaUdemyApplication.class, args);
+	}
+
+}
