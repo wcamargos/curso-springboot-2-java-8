97ef3e694b79fe6d72ed5efe84daacbc8e037efe
Welber Camargos <welber-camargos@hotmail.com>
2020-07-05 14:44:35 -0300

    Entidade User e Controller

---
+++ src/main/java/com/wnweb/curso/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.wnweb.curso.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wnweb.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "999981522", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
