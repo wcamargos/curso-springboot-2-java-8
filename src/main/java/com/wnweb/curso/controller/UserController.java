6f426875acf735862b4d647b0470473d4c88a3da
Welber Camargos <welber-camargos@hotmail.com>
2020-07-05 15:39:58 -0300

    Service

--- src/main/java/com/wnweb/curso/controller/UserController.java
+++ src/main/java/com/wnweb/curso/controller/UserController.java
@@ -1,19 +1,33 @@
 package com.wnweb.curso.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wnweb.curso.entities.User;
+import com.wnweb.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999981522", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findAll(@PathVariable Long id){
+		User obj = service.findbyId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/wnweb/curso/services/UserService.java
@@ -0,0 +1,30 @@
+package com.wnweb.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wnweb.curso.entities.User;
+import com.wnweb.curso.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findbyId(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get(); 
+ 	}
+	
+	
+	
+}
