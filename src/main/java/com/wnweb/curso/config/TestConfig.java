a033e8ead24ecf557575000b5b00e201459e012e
Welber Camargos <welber-camargos@hotmail.com>
2020-07-08 18:15:53 -0300

    Order Item

--- src/main/java/com/wnweb/curso/config/TestConfig.java
+++ src/main/java/com/wnweb/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.wnweb.curso.entities.Category;
 import com.wnweb.curso.entities.Order;
+import com.wnweb.curso.entities.OrderItem;
 import com.wnweb.curso.entities.Product;
 import com.wnweb.curso.entities.User;
 import com.wnweb.curso.entities.enums.OrderStatus;
 import com.wnweb.curso.repositories.CategoryRepository;
+import com.wnweb.curso.repositories.OrderItemRepository;
 import com.wnweb.curso.repositories.OrderRepository;
 import com.wnweb.curso.repositories.ProductRepository;
 import com.wnweb.curso.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -66,6 +71,15 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		
 		
 	}
 	
