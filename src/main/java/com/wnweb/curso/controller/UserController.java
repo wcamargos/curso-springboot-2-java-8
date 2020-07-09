0332ecc4f64cd6a867dabb747d4e2bc8cb88030d
Welber Camargos <welber-camargos@hotmail.com>
2020-07-09 19:49:59 -0300

    iserir user

--- src/main/java/com/wnweb/curso/controller/UserController.java
+++ src/main/java/com/wnweb/curso/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.wnweb.curso.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wnweb.curso.entities.User;
 import com.wnweb.curso.services.UserService;
@@ -29,5 +33,12 @@ public class UserController {
 	public ResponseEntity<User> findAll(@PathVariable Long id){
 		User obj = service.findbyId(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping(value = "/insert")
+	public ResponseEntity<User> insert(@RequestBody User obj){  //RequestBody serve pra serializar o Json vindo do front para Obj
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }
--- src/main/java/com/wnweb/curso/services/UserService.java
+++ src/main/java/com/wnweb/curso/services/UserService.java
@@ -25,6 +25,10 @@ public class UserService {
 		return obj.get(); 
  	}
 	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
 	
 	
 }
